5ed45a61d0b85aa4ca0b87041f7c60b60f050ed4
julien.le.huludut@gmail.com <julien.le.huludut@gmail.com>
2016-10-09 14:04:28 +0200

    added dropbox

--- borg_simple_backup.sh
+++ borg_simple_backup.sh
@@ -6,18 +6,19 @@ echo "
 
 
 
-##### starting backup at $DATE (real date is $(timeout 1 curl -G --data-urlencode \"format=%D-%T\n\" http://www.timeapi.org/cest/1+hours+before+now?)) #####"     >> $LOG 2>&1
+##### starting backup at $DATE (real date is $(timeout 2 curl -G --data-urlencode \"format=%D-%T\n\" http://www.timeapi.org/cest/1+hours+before+now?)) #####"     >> $LOG 2>&1
+dropbox stop >> $LOG 2>&1|| failed to stop dropbox  >> $LOG 2>&1
 
 borg create                           \
     $REPOSITORY::'{hostname}-{now}'    \
-    --exclude-from /etc/borg.exclusion		\
     --compression lzma,9                        \
     --verbose --stats                           \
     $DATA                                       \
     >> $LOG 2>&1
 
 if [[ $? -eq 2 ]]
-	then echo "aborted, already exists. exiting."  >> $LOG
+	then echo "aborted. exiting."  >> $LOG
+	dropbox start
 	exit 127
 fi
 
@@ -25,7 +26,12 @@ echo "##### ending backup at $DATE  #####
 ... pruning
  "     >> $LOG 2>&1
 
-borg prune --debug --list --prefix '{hostname}-' --keep-within=1H --keep-hourly=1 --keep-daily=2 --keep-weekly=1 --keep-monthly=1 $REPOSITORY >> $LOG 2>&1|| echo "pruning failed">> $LOG
+borg prune 
+    --debug --list 				\
+    --prefix '{hostname}-' 			\
+    --keep-within=1H --keep-hourly=1 --keep-daily=2 --keep-weekly=1 --keep-monthly=1 \
+    $REPOSITORY 				\
+    >> $LOG 2>&1|| echo "pruning failed">> $LOG
 
 
 echo "
@@ -33,6 +39,10 @@ echo "
 ##### done.               #####
 
 "     >> $LOG 2>&1
+
+dropbox start >> $LOG 2>&1 || failed to start dropbox  >> $LOG 2>&1
+dropbox status >> $LOG 2>&1
+
 
 echo "##### starting 1st transfer at $DATE #####"     >> $LOG 2>&1
 drive -c /home/jlehulud/.gdrive sync upload $REPOSITORY 0B3ZbmRr1RRkHVGlObFdHTzN2QVU >> $LOG 2>&1
@@ -43,7 +53,9 @@ echo "
 
 "     >> $LOG 2>&1
 
+dropbox status >> $LOG 2>&1
+
 drive -c /home/jlehulud/.gdrive-secondary sync upload $REPOSITORY 0B8HiID_B112OSzdNZ2tOeHRRSDQ >> $LOG 2>&1
 
-echo "##### done 2nd transfer at $DATE (real date is $(timeout 1 curl -G --data-urlencode \"format=%D-%T\n\" http://www.timeapi.org/cest/1+hours+before+now?)) #####"     >> $LOG 2>&1
+echo "##### done 2nd transfer at $DATE (real date is $(timeout 2 curl -G --data-urlencode \"format=%D-%T\n\" http://www.timeapi.org/cest/1+hours+before+now?)) #####"     >> $LOG 2>&1
 
